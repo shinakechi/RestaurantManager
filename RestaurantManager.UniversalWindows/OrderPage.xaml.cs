80857ef1e4b7f360d260b3243a201a5e6bad18c6
shinakechi <shin.akechi@outlook.com>
2015-10-26 18:09:52 -0700

    RestaurantManager
    Edx.org XAML Beginner Course solution

---
+++ RestaurantManager.UniversalWindows/OrderPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace RestaurantManager.UniversalWindows
+{
+    public sealed partial class OrderPage : Page
+    {
+        public OrderPage()
+        {
+            this.InitializeComponent();
+        }
+
+        void GoBack2MainPage_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(MainPage));
+        }
+    }
+
+
+    
+}
